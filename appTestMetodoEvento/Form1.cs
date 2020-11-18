1afca6946e31c3e3f17c2d48d00099e39f3524dd
MONICA ADAMARI AVALOS MU�OZ <I20050003@monclova.tecnm.mx>
2020-11-18 12:08:54 -0600

    agregue botones

---
+++ appTestMetodoEvento/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appTestMetodoEvento
+{
+    public partial class Form1 : Form
+    {
+        //Var Globales
+        double valor1, valor2;
+        string operacion = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //private void btn0_Click(object sender, EventArgs e)
+        //{
+        //    lblNumeros.Text += "0";
+        //}
+
+        //private void btn1_Click(object sender, EventArgs e)
+        //{
+        //    lblNumeros.Text += "1";
+        //}
+
+        private void MiMetodo_Click(object sender, EventArgs e)
+        {
+            string textoBoton = ((Button)sender).Text;
+            switch (textoBoton)
+            {
+                case "+":
+                    if (lblNumeros.Text == "")
+                    {
+                        break;
+                    }
+                    operacion = textoBoton;
+                    valor1 = double.Parse(lblNumeros.Text);
+                    lblNumeros.Text = "";
+                    break;
+                case "-":
+                    if (lblNumeros.Text == "")
+                    {
+                        break;
+                    }
+                    operacion = textoBoton;
+                    valor1 = double.Parse(lblNumeros.Text);
+                    lblNumeros.Text = "";
+                    break;
+                case "X":
+                    if (lblNumeros.Text == "")
+                    {
+                        break;
+                    }
+                    operacion = textoBoton;
+                    valor1 = double.Parse(lblNumeros.Text);
+                    lblNumeros.Text = "";
+                    break;
+                case "/":
+                    if (lblNumeros.Text == "")
+                    {
+                        break;
+                    }
+                    operacion = textoBoton;
+                    valor1 = double.Parse(lblNumeros.Text);
+                    lblNumeros.Text = "";
+                    break;
+                case "=":
+                    if (lblNumeros.Text=="")
+                    {
+                        break;
+                    }
+                    switch (operacion)
+                    {
+                        case "+":
+                    valor2 = double.Parse(lblNumeros.Text);
+                    lblNumeros.Text = (valor1 + valor2) + "";
+                    break;
+                        case "-":
+                            valor2 = double.Parse(lblNumeros.Text);
+                            lblNumeros.Text = (valor1 - valor2) + "";
+                            break;
+                        case "X":
+                            valor2 = double.Parse(lblNumeros.Text);
+                            lblNumeros.Text = (valor1 * valor2) + "";
+                            break;
+                        case "/":
+                            valor2 = double.Parse(lblNumeros.Text);
+                            lblNumeros.Text = (valor1 / valor2) + "";
+                            break;
+                    }
+                    break;
+
+                case "AC":
+                    lblNumeros.Text = "";
+                    break;
+                case ".":
+                    if (lblNumeros.Text.IndexOf(".") == -1 )
+                    {
+                        lblNumeros.Text += textoBoton;
+                    }
+                    break;
+                default:
+                    lblNumeros.Text += textoBoton;
+                break;
+            }
+
+            //lblNumeros.Text += textoBoton;
+
+            //string saludo = "Hola";
+
+            //saludo = saludo + "buen dia";
+
+            //saludo += "buen dia";
+
+            //// + string .. CONCATENAR .. UNIR :: PEGAR :: JUNTAR
+        }
+
+        // int x=1;
+        // x=x+1; ...  x+=1; ... x++; ... ++x;
+        // x=x-1; ...  x-=1; ... x--; ... --x;
+        // x=x/1; ...  x/=1; ... x//; ... //x;
+        // int edad = 33;
+        //
+    }
+}
